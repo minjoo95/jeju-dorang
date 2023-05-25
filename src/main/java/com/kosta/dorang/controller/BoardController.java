b801b43336b0755e59f97556cecdd5a3aa04cde8
fffso <f5fuiop@gmail.com>
2023-05-25 18:06:55 +0900

    board controller 수정

--- src/main/java/com/kosta/dorang/controller/BoardController.java
+++ src/main/java/com/kosta/dorang/controller/BoardController.java
@@ -1,8 +1,11 @@
 package com.kosta.dorang.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/board")
 public class BoardController {
 
+	
 }
